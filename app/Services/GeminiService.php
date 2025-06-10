8f0195e97e70b765a0e3716c75d25aee310cacca
João Paulo Abdala Bohaczk <114778253+joaopabdala@users.noreply.github.com>
2025-06-10 15:12:09 -0300

    cleaning unused imports (#18)

--- app/Services/GeminiService.php
+++ app/Services/GeminiService.php
@@ -3,11 +3,9 @@
 namespace App\Services;
 
 use Exception;
-use Illuminate\Http\File;
 use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
-use function base64_decode;
 use function base64_encode;
 use function config;
 use function report;
@@ -39,7 +37,7 @@ class GeminiService
                 'contents' => [[
                     'parts' => [
                         [
-                        'text' => 'Transcreva o conteúdo da imagem mantendo a formatação. A imagem é uma página de livro ou de uma revista. Foque somente na página principal, ignore o conteúdo das bordas. O conteúdo deve ser retornado em json classificando o título, subtítulo, o número da página, qualquer header com o título do livro/capítulo. O conteúdo principal deve ser retornado por parágrafos, mantendo ao máximo a estrutura original do texto, mas as quebras de linha originais não precisam ser especificadas como quebras, o importante é saber o início e fim dos parágrafos, "\n" e "\t" não devem ser retornados. No caso de tabulações no formato de entrevistas separar o nome do falante como se fosse um novo parágrafo. Caso alguma dessas informações esteja faltando deve ser retornado null. Caso a imagem tenha mais de uma página o conteúdo deve retornar várias páginas. Exemplo de construção do json:
+                            'text' => 'Transcreva o conteúdo da imagem mantendo a formatação. A imagem é uma página de livro ou de uma revista. Foque somente na página principal, ignore o conteúdo das bordas. O conteúdo deve ser retornado em json classificando o título, subtítulo, o número da página, qualquer header com o título do livro/capítulo. O conteúdo principal deve ser retornado por parágrafos, mantendo ao máximo a estrutura original do texto, mas as quebras de linha originais não precisam ser especificadas como quebras, o importante é saber o início e fim dos parágrafos, "\n" e "\t" não devem ser retornados. No caso de tabulações no formato de entrevistas separar o nome do falante como se fosse um novo parágrafo. Caso alguma dessas informações esteja faltando deve ser retornado null. Caso a imagem tenha mais de uma página o conteúdo deve retornar várias páginas. Exemplo de construção do json:
                          {
                             "page" : {
                                 "headerTitle": null
@@ -70,9 +68,9 @@ class GeminiService
                 throw new Exception("Gemini API Error: " . $response->status() . " - " . $response->body());
             }
         } catch (Exception $e) {
-             report($e);
-             Log::error($e->getMessage());
-             return response()->json('Error fetching API', 500);
+            report($e);
+            Log::error($e->getMessage());
+            return response()->json('Error fetching API', 500);
         }
     }
 
